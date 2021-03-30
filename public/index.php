f1190c2a0cd7e0771a8e6ca5fcef8ea879ce820e
th0m4sm0ss3t <mosset.thomas@gmail.com>
2021-03-30 16:02:50 +0200

    mise en place des routes avec altorouter + dispatch

--- public/index.php
+++ public/index.php
@@ -10,23 +10,56 @@ require __DIR__.'/../app/Controllers/StoryController.php';
 // On veut inclure la bonne page selon le paramètre "page"
 // reçu en paramètre GET
 
-if (!empty($_GET['_url'])) {
-    $currentUrl = $_GET['_url'];
-} else {
-    $currentUrl = '/';
-}
+// Instanciation de notre object $router
+$router = new AltoRouter();
+// On définit le chemin vers dossier public
+$router->setBasePath('/Revisions/PHP/TP-MVC/public');
 
-if ($currentUrl === '/') {
-    // On instancie un objet $controller
-    $controller = new MainController();
-    // On appelle la méthode souhaitée, ici home
-    $controller->home();
-}
-elseif ($currentUrl === '/list') {
-    // On instancie un objet $controller
-    $controller = new StoryController();
-    // On appelle la méthode souhaitée
-    $controller->storiesList();
+/* HOME */
+$router->map(
+    'GET', // méthode HTTP
+    '/', // url
+    [
+        'controller' => 'MainController', // destination vers controller à utiliser
+        'method' => 'home', // destination vers méthode du controller à utiliser
+    ], 
+    'home' // nom de la route
+);
+
+/* STORIES LIST */
+$router->map(
+    'GET', 
+    '/list', 
+    [
+        'controller' => 'StoryController',
+        'method' => 'storiesList',
+    ], 
+    'list'
+);
+
+// On vérifie s'il y a un match
+// Renvoi soit false, soit infos demandées
+$match = $router->match();
+
+dump($match);
+
+if ($match !== false) {
+    // dump($match['target']);
+    /* DONNE : array:2 [
+            "controller" => "MainController"
+            "method" => "home"
+        ]
+    */
+    // on appelle notre page
+    // Via le controller ...
+    $controllerName = $match['target']['controller'];
+    // ... et sa méthode
+    $methodName = $match['target']['method'];
+
+    // On instancie un objet $controller dynamiquement
+    $controller = new $controllerName();
+    // On appelle la méthode souhaitée dynamiquement
+    $controller->$methodName();
 }
 else {
     // Alors page non trouvée => 404
